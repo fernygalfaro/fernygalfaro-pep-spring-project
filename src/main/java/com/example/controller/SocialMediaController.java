e9edc4258a1533e874fe690379317e8f7af425ff
Fernando Alfaro <fernandogalfaro@lewisu.edu>
2024-12-05 19:49:17 +0000

    WIP

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -2,16 +2,24 @@ package com.example.controller;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.dom4j.IllegalAddException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Account;
+import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
+import java.util.List;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -45,5 +53,57 @@ public class SocialMediaController {
         }catch(IllegalArgumentException e){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+    @PostMapping("/messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message){
+        try{
+            return ResponseEntity.ok(messageService.createMessage(message));
+        }
+        catch(IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    @GetMapping("/messages")
+    public List<Message> getAllMessages (){
+        return messageService.getAllMessages();
+    }
+    @GetMapping("/messages/{messageId}")
+    public ResponseEntity<Message> getMessageById(@PathVariable Integer id){
+      Message message = messageService.getMessageById(id);
+      if(message == null){
+        return ResponseEntity.ok().build();
+      }
+      return ResponseEntity.ok(message);
+    }
+    @PatchMapping("/messages/{messageId}")
+    public ResponseEntity<Integer> updateMessage(@PathVariable Integer messageId, @RequestBody String newText){
+        try {
+            if(newText.isBlank() || newText.length() > 255){
+                return ResponseEntity.badRequest().body(null);
+            }
+            int rowsUpdated = messageService.updateMessageText(messageId, newText);
+            if(rowsUpdated == 1){
+                return ResponseEntity.ok(rowsUpdated);
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().build();
+        }
+    } 
+    @GetMapping("/accounts/{accountId}/messages")
+    public List<Message> getMessageByUser(@PathVariable Integer accountId){
+        return messageService.getMessagesByUserId(accountId);
+    }   
+    @DeleteMapping("/messages/{messageId}")
+    public ResponseEntity<Integer> deleteMessage(@PathVariable Integer messageId){
+       
+           int rowsUpdated = messageService.deleteMessage(messageId);
+            if(rowsUpdated == 0){
+                return ResponseEntity.ok().build();
+            }
+            return ResponseEntity.ok(rowsUpdated);
+       
     }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -11,27 +11,46 @@ import java.util.List;
 public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
-    
+    @Autowired
+    private AccountRepository accountRepository;
 
     public Message createMessage(Message message){
+        if(message.getMessageText().isBlank()|| message.getMessageText().length() > 255){
+            throw new IllegalArgumentException("Invalid");
+        }
+        if (!accountRepository.existsById(message.getPostedBy())){
+            throw new IllegalArgumentException("Invalid");
+        }
         return messageRepository.save(message);
     }
     public List<Message> getAllMessages(){
         return messageRepository.findAll();
     }
     public Message getMessageById(Integer id){
-        return messageRepository.getById(id);
+        return messageRepository.findById(id).orElse(null);
     }
-    public void deleteMessageById(Integer id){
-        messageRepository.deleteById(id);
-    }
-    public Message updateMessageText (Integer id, String newText){
-        Message message = getMessageById(id);
+    
+    public int updateMessageText (Integer id, String newText){
+        Message message = messageRepository.findById(id).orElse(null);
+        if (message == null){
+            return 0;
+        }
+        if(newText == null || newText.trim().isEmpty() || newText.length() > 255){
+            throw new IllegalArgumentException("Invalid");
+        }
         message.setMessageText(newText);
-        return messageRepository.save(message);
+        messageRepository.save(message);
+        return 1;
     }
     public List<Message> getMessagesByUserId(Integer userId){
         return messageRepository.findByPostedBy(userId);
+    }
+    public int deleteMessage(Integer messageId){
+        if(!messageRepository.existsById(messageId)){
+            return 0;
+        }
+        messageRepository.deleteById(messageId);
+        return 1;
     }
 
 
